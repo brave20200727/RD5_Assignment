441241457cd93563e0e53735396caf5400747364
Brave <brave_chen@mail.chungyo.net>
2020-08-28 17:57:32 +0800

    調整mainPage.html中card的功能

--- Api.php
+++ Api.php
@@ -2,4 +2,7 @@
     if(isset($_POST["loginButton"])) {
         header("Location: mainPage.html");
     }
+    if(isset($_POST["signUpButton"])) {
+        header("Location: index.html");
+    }
 ?>
